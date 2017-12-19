5633b645661642f3c97fa60e7ad390d7a408b1a9
Emmeris <jonas.lagaude@yahoo.de>
2017-12-19 15:55:57 +0100

    add methods

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "crypto.h"
 
+int encrypt(KEY key, const char* input, char* output) {
+	return 0;
+}
+
+int decrypt(KEY key, const char* cypherText, char* output) {
+	return 0;
+}
+
 int main(int argc, const char* argv[]){
-	printf("hello wordl");
+	printf("hello word");
 	return 0;
 }
